c42c069767fdb05dc8a35082d138aaf30832cd41
Jone23 <jone23@ramble.ru>
2022-04-27 12:22:10 +0300

    add fun getNull

--- src/StudyKotlin.kt
+++ src/StudyKotlin.kt
@@ -2,5 +2,19 @@ fun main(args: Array<String>){
 
     println("Привет новый день")
 
-}
 
+    getNull()
+
+}
+fun getNull(){
+
+    var str1:String?="Jone"
+    println(str1?.length)
+
+    str1=null
+
+    println(str1?.length)
+
+
+
+}
